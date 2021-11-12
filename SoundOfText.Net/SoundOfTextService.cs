b267e154fb511ef4ad1b8db8f6f995ecd7c6e5b1
Dan Manning <dmanning@oceaneering.com>
2021-11-12 16:31:05 -0600

    added better error checking

--- SoundOfText.Net/SoundOfTextService.cs
+++ SoundOfText.Net/SoundOfTextService.cs
@@ -15,17 +15,37 @@ namespace SoundOfText.Net
 			//Check for 
 			if (!postResponse.success)
 			{
-				throw new Exception("Call to api.soundoftext.com/sounds failed");
+				throw new Exception($"POST Call to api.soundoftext.com/sounds failed: {postResponse.message}");
 			}
 
 			var getResponse = await $"https://api.soundoftext.com/sounds/{postResponse.id}".GetJsonAsync();
 
+			if (getResponse.status == "Pending")
+			{
+				while (getResponse.status == "Pending")
+				{
+					//wait a second
+					await Task.Delay(500);
+
+					//try again
+					getResponse = await $"https://api.soundoftext.com/sounds/{postResponse.id}".GetJsonAsync();
+
+					if (getResponse.status != "Pending")
+					{
+						break;
+					}
+				}
+			}
+			
 			if (getResponse.status != "Done")
 			{
-				throw new Exception("Call to api.soundoftext.com/sounds failed");
+				throw new Exception($"GET Call to api.soundoftext.com/sounds failed: {getResponse.message}");
 			}
+
 			return getResponse.location;
 		}
+
+
 
 		public async Task Download(string sourceFileLocation, string localFolderPath, string localFileName = null)
 		{
